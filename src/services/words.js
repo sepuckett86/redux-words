9d3bb5cdcefb2178627808bb4d4c578d803e760c
sepuckett86 <sepuckett86@gmail.com>
2019-08-27 14:03:28 -0700

    try to build words service

---
+++ src/services/words.js
@@ -0,0 +1,12 @@
+export const getWords = (limit) => {
+  fetch('https://gist.githubusercontent.com/mehtaphysical/ed50c7079ef211dd1f395e08db69ac7b/raw/a9d710e083d57fc01b75b77370127d68de3864a5/words.json')
+    .then(res => {
+      if(!res.ok) throw 'Unable to fetch stuff bruh :(';
+      return res.json();
+    })
+    .then(res => {
+      const shortened = res.slice(0, limit);
+      return shortened;
+    });
+};
+
